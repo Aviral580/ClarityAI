27c0137b9ab54529ddd8ce52dd68b68ab4cbd106
Venu4i <venuverma04@gmail.com>
2026-01-09 20:38:26 +0530

    fix : "bug"

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -22,7 +22,7 @@ import taskRouter from './routes/task.routes.js';
 //declaration of routes
 app.use("/api/auth",userRouter);
 app.use("/api/v1/ai", aiRouter);
-app.use("api/tasks", taskRouter)
+app.use("/api/tasks", taskRouter)
 
 app.use((err,_, res, next) => {
   console.error(" Error Handler:", err);
